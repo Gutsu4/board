2b7abdbbfe639b3ebe5ceafe00c1d7faaa27e762
Yuya <yuya08420bs@gmail.com>
2024-10-12 02:01:40 +0900

    エラーメッセージ修正とヘッダーの画像サイズ修正

--- resources/views/components/header-guest.blade.php
+++ resources/views/components/header-guest.blade.php
@@ -1,17 +1,16 @@
-<nav class="flex justify-between items-center h-14 bg-white px-2">
+<nav class="flex justify-between items-center h-14 md:h-16 bg-white px-2 md:px-4">
     <a href="" class="h-full">
         <img class="lg:pr-4 h-full" src="{{ asset('images/board-logo.png') }}" alt="Logo">
     </a>
     <h1 class="mr-auto text-16 lg:text-24">匿名質問版</h1>
     <div class="flex gap-2 md:gap-3 items-center">
-        <button
-            class="text-12 md:text-16 text-gray-dark font-bold border border-gray rounded-md px-2 py-1.5 md:px-2.5 md:py-2"
-            onclick="window.location='{{route('login')}}'"
+        <button class="text-14 md:text-16 text-gray-dark font-bold border border-gray rounded-md px-2.5 py-2"
+                onclick="window.location='{{route('login')}}'"
         >
             ログイン
         </button>
         <button
-            class="text-12 md:text-16 text-white font-bold border border-gradient-to-r bg-gradient-to-r from-[#0097DF] to-[#015FDB] rounded-md px-2 py-1.5 md:px-8 md:py-2.5"
+            class="text-14 md:text-16 text-white font-bold border border-gradient-to-r bg-gradient-to-r from-[#0097DF] to-[#015FDB] rounded-md px-2.5 md:px-8 py-2.5"
             onclick="window.location='https://app.slack.com/client/T02EJDCD7FA/C06AK9H46LR'"
         >
             Slackへ問い合わせ
